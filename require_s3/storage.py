fabb412907a9c8d7e42b455da9ece958ce55b1a8
Ben Miller <ben@hyl.co.uk>
2014-01-10 10:38:04 +0000

    re-escapes the url coming out of Boto that has been unquoted by CachedFilesMixin

--- require_s3/storage.py
+++ require_s3/storage.py
@@ -1,3 +1,5 @@
+import urlparse, urllib
+
 from django.contrib.staticfiles.storage import CachedFilesMixin
 
 from storages.backends.s3boto import S3BotoStorage
@@ -15,6 +17,20 @@ class FolderNameHackMixin(object):
         return url
 
 
+class NormalizeUrlHackMixin(object):
+
+    # HACK: re-escapes the url coming out of Boto that has been unquoted by CachedFilesMixin
+    # from http://stackoverflow.com/questions/11820566/inconsistent-signaturedoesnotmatch-amazon-s3-with-django-pipeline-s3boto-and-st
+    def url(self, name, *args, **kwargs):
+        url = super(NormalizeUrlHackMixin, self).url(name, *args, **kwargs)
+        if isinstance(url, unicode):
+            url = url.encode('utf-8', 'ignore')
+        scheme, netloc, path, qs, anchor = urlparse.urlsplit(url)
+        path = urllib.quote(path, '/%')
+        qs = urllib.quote_plus(qs, ':&=')
+        return urlparse.urlunsplit((scheme, netloc, path, qs, anchor))
+
+
 class OptimizedStaticFilesStorage(FolderNameHackMixin, OptimizedFilesMixin, S3BotoStorage):
     
     """
@@ -22,7 +38,7 @@ class OptimizedStaticFilesStorage(FolderNameHackMixin, OptimizedFilesMixin, S3Bo
     """
 
 
-class OptimizedCachedStaticFilesStorage(FolderNameHackMixin, OptimizedFilesMixin, CachedFilesMixin, S3BotoStorage):
+class OptimizedCachedStaticFilesStorage(NormalizeUrlHackMixin, FolderNameHackMixin, OptimizedFilesMixin, CachedFilesMixin, S3BotoStorage):
     
     """
     A storage backend that optimizes with RequireJS, applies a cached MD5 suffix,
